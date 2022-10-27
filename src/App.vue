21717e91e964024019dd4982e1f267e8b5db3e51
PWCTW\JYKL <jackyleeidea@gmail.com>
2022-10-27 12:10:14 +0800

    feat:改以Interaction模式建置python tool, 更新版本號 0.2.1

--- src/App.vue
+++ src/App.vue
@@ -96,7 +96,7 @@
   </div>
 
   <footer class="footer mt-auto">
-    <p class="text-muted" style="margin: 0px;text-align: center;">版本：0.2.0</p>
+    <p class="text-muted" style="margin: 0px;text-align: center;">版本：0.2.1</p>
   </footer>
 
 </template>
